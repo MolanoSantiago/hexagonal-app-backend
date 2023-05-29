e915c282016fde7aee5956966c816b0443c8d6ea
MolanoSantiago <molanosantiagoplay@gmail.com>
2023-05-29 12:15:09 -0500

    Add services for management api routes

---
+++ src/Shared/Infrastructure/Services/DependencyServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Src\Shared\Infrastructure\Services;
+
+use Illuminate\Support\ServiceProvider as Service;
+
+class DependencyServiceProvider extends Service
+{
+
+    private array $dependencies;
+
+    public function setDependecy(array $dependencies): void
+    {
+        $this->dependencies = $dependencies;
+    }
+
+    public function register(): void
+    {
+        foreach ($this->dependencies as $value) {
+            $this->app
+                ->when($value['useCase'])
+                ->needs($value['contract'])
+                ->give($value['repository']);
+        }
+    }
+}
