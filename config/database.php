1495ad9ebb47d1f3e8c19e8da695f8f8061981d6
MolanoAlternova <santiago.molano@alternova.com>
2023-06-07 22:53:02 -0500

    asdfasredis

--- config/database.php
+++ config/database.php
@@ -134,7 +134,15 @@ return [
                     'database' => 0,
                 ],
             ],
+            'cache' => [
+                [
+                    'host' => env('REDIS_CACHE_HOST', '127.0.0.1'),
+                    'password' => env('REDIS_CACHE_PASSWORD', null),
+                    'port' => env('REDIS_CACHE_PORT', 6379),
+                    'database' => 1,
+                ],
+            ],
         ],
-    ],    
+    ],     
 
 ];
